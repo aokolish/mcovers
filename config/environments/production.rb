c9cccf42897bace86d12fb34d062b0ed4d4cb333
Alex Okolish <alex.okolish@gmail.com>
2011-03-29 21:57:28 -0400

    setup cache dir for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ MessinaCovers::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_controller.page_cache_directory = Rails.root.to_s + "/public/cache"
 end
