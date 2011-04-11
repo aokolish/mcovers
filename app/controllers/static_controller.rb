c9821214077e6344083091ada37274f968d39f5f
Alex Okolish <alex.okolish@gmail.com>
2011-04-10 23:16:31 -0400

    Added a page title variable to the static controller. Thinking about adding a keywords variable so that each page can have different keywords for SEO purposes.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,21 +2,29 @@ class StaticController < ApplicationController
   caches_page :index, :construction, :gallery, :distributors, :sizes, :custom   
     
   def index
+    @title = 'home'
+    #TODO: put keywords here for SEO
+    @keywords = ''  
   end
 
   def construction
+    @title = 'construction'
   end
 
   def gallery
+    @title = 'gallery'
   end
 
   def distributors
+    @title = 'distributors'
   end
 
   def sizes
+    @title = 'sizes'
   end
 
   def custom
+    @title = 'custom'
   end
 
 end
