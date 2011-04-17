d94be920c05c5dad56819b59df4fd0238c56d059
Alex Okolish <alex.okolish@gmail.com>
2011-04-16 23:52:49 -0400

    google maps js now uses a json object to get the info about each store. refactored js in general.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,66 +1,76 @@
-
 function initialize() {
-	infowindow = new google.maps.InfoWindow({ pixelOffset: new google.maps.Size(0, -34) });
-  var myLatlng = new google.maps.LatLng(37.09, -95.71),
+    var infowindow = new google.maps.InfoWindow({
+        pixelOffset: new google.maps.Size(0, -34),
+    }),
+    myLatlng = new google.maps.LatLng(37.09, -95.71),
+    markers = [],
 
-  		myOptions = {
-	   		zoom: 4,
-		    center: myLatlng,
-		    mapTypeId: google.maps.MapTypeId.ROADMAP
-		  },
-		
-			map = new google.maps.Map(document.getElementById("map_canvas"), myOptions),
-			
-			markers = [ a = {latlng: new google.maps.LatLng(39.1484956, -84.598031), 
-											title: 'The Cincinnati Bass Cellar' },
-		 						b = {latlng: new google.maps.LatLng(41.662427, -91.512043), 
-											title: 'Heartland String Bass Shop' },
-								c = {latlng: new google.maps.LatLng(39.1628618, -84.5398662), 
-											title: 'Nick LLoyd Basses', phone: '513-681-1863' },
-								d = {latlng: new google.maps.LatLng(41.1641135, -73.4249774), title: 'd' },
-								e = {latlng: new google.maps.LatLng(42.964613, -85.662771), 
-											title: 'Steven Reiley at Guaneri House' },
-								f = {latlng: new google.maps.LatLng(37.066083,  -122.078062), title: 'f' },
-								g = {latlng: new google.maps.LatLng(38.987076, -76.792751), title: 'g' },
-								h = {latlng: new google.maps.LatLng(37.8056014, -122.2743443), title: 'h' },
-								i = {latlng: new google.maps.LatLng(40.0560649, -83.0194269), title: 'i' }];
+    myOptions = {
+        zoom: 4,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    },
 
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-  for (var i = 0; i < markers.length; i++) {
-			createMarker(markers[i]);
-   }
+		// distributors obj from distributors.html page
+		// for each one push onto array of markers and create a marker for the map
+		for (i in distributors) {
+			markers.push(createMarker(distributors[i], i));
+		}
 
-  google.maps.event.addListener(map, "click", function() {
-		infowindow.close();
-	});
-
-  function createMarker(m) {
-	  var markerContent = '<b>' + m.title + '</b><br /><br />' + m.phone + 
-			'<br /><br /><a href="#">Get Directions</a>';
-    var marker = new google.maps.Marker({position: m.latlng, map: map});
-		marker.content = markerContent;
-    google.maps.event.addListener(marker, "click", function() {
-      if (infowindow) infowindow.close();
-			infowindow.setPosition(marker.position);
-    	infowindow.setContent(marker.content);
-      infowindow.open(map);
+    google.maps.event.addListener(map, "click", function() {
+        infowindow.close();
     });
-    return marker;
-  }
-}
+
+    function createMarker(store, i) {
+        store.latlng = new google.maps.LatLng(store.lat, store.lon);
+        var marker = new google.maps.Marker({
+            position: store.latlng,
+            map: map
+        });
+        marker.content = getInfoHTML(store);
+        google.maps.event.addListener(marker, "click", function() {
+            if (infowindow) infowindow.close();
+            infowindow.setPosition(marker.position);
+            infowindow.setContent(marker.content);
+            infowindow.open(map);
+        });
+        return marker;
+    }
+
+    function getInfoHTML(store) {
+        // constructs markup for the info window (used for each marker)
+        var content = $("div.infowindow.template").clone().removeClass("template"),
+						href = "http://maps.google.com/maps?saddr=&daddr=" + store.address1 + "," + store.address2;
+        content.find("p.store-name b").text(store.name);
+        content.find("p.address-1").text(store.address1);
+				content.find("p.address-2").text(store.address2);
+        content.find("p.store-phone").text(store.phone1);
+        content.find("a").attr("href", href);
+        content = content.html();
+        return content;
+    }
+}				// end initialize
 
 $(document).ready(function() {
-	if ($('body').hasClass('distributors')) {
-		initialize();
-		
-		// Accordion
-		$("#accordion").accordion({ header: "h3", autoHeight: false });
+    if ($('body').hasClass('distributors')) {
+        initialize();
 
-		//hover states on the static widgets
-		$('#dialog_link, ul#icons li').hover(
-			function() { $(this).addClass('ui-state-hover'); }, 
-			function() { $(this).removeClass('ui-state-hover'); }
-		);
-	} 
+        // Accordion
+        $("#accordion").accordion({
+            header: "h3",
+            autoHeight: false,
+						collapsible: true
+        });
+
+        // hover states on the static widgets
+        $('#dialog_link, ul#icons li').hover( function() {
+		            $(this).addClass('ui-state-hover');
+		        }, function() {
+		            $(this).removeClass('ui-state-hover');
+		        }
+        );
+    }
 
 });
