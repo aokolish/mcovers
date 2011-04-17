c2f52df33fcce765a08d9873fd9458c71763437b
Alex Okolish <alex.okolish@gmail.com>
2011-04-17 13:42:03 -0400

    removed comments from distributor sorting code. added code to sort and make available an array of sizes based on a yaml file. probably, not the cleanest solution, but it does seem to be working.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
 class StaticController < ApplicationController
   caches_page :index, :construction, :gallery, :distributors, :sizes, :custom   
   require 'yaml'
+  
+  # TODO: get rounded corners working in older versions of IE
+  # there has to be a reasonable way of doing this
     
   def index
     @title = 'home'
@@ -17,15 +20,20 @@ class StaticController < ApplicationController
   end
 
   def distributors
-    @title = 'distributors'
-    @distributors = YAML.load_file("#{RAILS_ROOT}/config/distributors.yml")
-    @distributors = @distributors.values.sort do |x, y|                              #in this case, the keys are meaningless. just want the values.                                                           
-      x['name'].sub(/^(the|a|an)\s/i, '') <=> y['name'].sub(/^(the|a|an)\s/i, '')    # sort them by their names (ignoring some words like 'the')  
+    @title = 'distributors'          
+    @distributors = YAML.load_file("#{RAILS_ROOT}/config/distributors.yml").values.sort do |x, y|                                                                                         
+      x['name'].sub(/^(the|a|an)\s/i, '') <=> y['name'].sub(/^(the|a|an)\s/i, '')    
     end
   end
 
   def sizes
     @title = 'sizes'
+    @sizes = YAML.load_file("#{RAILS_ROOT}/config/sizes.yml").sort {|a,b| a[0]<=>b[0]}
+    
+    #done with the keys, just need them for the purpose of sorting my hash
+    #flatten array, then pull out the even values (the hashes)
+    @sizes = @sizes.flatten   
+    @sizes = @sizes.values_at(*(1...@sizes.size).step(2))
   end
 
   def custom
