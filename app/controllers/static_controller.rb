4bc13f2d752102a7f0c3c2fc6a4a98b97743f35e
Alex Okolish <alex.okolish@gmail.com>
2011-04-16 23:56:15 -0400

    using .values allows me to flatten this array and to send only what I need to the view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -19,8 +19,8 @@ class StaticController < ApplicationController
   def distributors
     @title = 'distributors'
     @distributors = YAML.load_file("#{RAILS_ROOT}/config/distributors.yml")
-    @distributors = @distributors.sort do |x, y|                                   
-      x[1]['name'].sub(/^(the|a|an)\s/i, '') <=> y[1]['name'].sub(/^(the|a|an)\s/i, '')    # sort them by their names (ignoring some words like 'the')  
+    @distributors = @distributors.values.sort do |x, y|                              #in this case, the keys are meaningless. just want the values.                                                           
+      x['name'].sub(/^(the|a|an)\s/i, '') <=> y['name'].sub(/^(the|a|an)\s/i, '')    # sort them by their names (ignoring some words like 'the')  
     end
   end
 
