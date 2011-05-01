1e0c91214dd01bf8743bef99ea51b5afaffd0fcc
Alex Okolish <alex.okolish@gmail.com>
2011-05-01 17:55:00 -0400

    removed comments from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,7 @@ MessinaCovers::Application.routes.draw do
 
   match '/custom' => 'static#custom', :as => :custom
   
-  # map root to static controller
   root :to => "static#index", :as => :index
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
 
   # See how all your routes lay out with "rake routes"
 end
